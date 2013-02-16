0434418f88ea4a57371ca2794eef8b426922818c
John Mastro <john.b.mastro@gmail.com>
2013-02-15 21:06:13 -0800

    Don't enable auto-fill in GFM mode

--- init-markdown.el
+++ init-markdown.el
@@ -6,6 +6,13 @@
           (add-to-list 'auto-mode-alist (cons ext 'markdown-mode)))
       (list "\\.markdown" "\\.mkd" "\\.md"))
 
-(add-hook 'markdown-mode-hook 'turn-on-auto-fill)
+(defun maybe-turn-on-auto-fill ()
+  (unless (eq major-mode 'gfm-mode)
+    (turn-on-auto-fill)))
+
+(add-hook 'markdown-mode-hook 'maybe-turn-on-auto-fill)
+
+(eval-after-load 'markdown
+  '(basis/define-hyper markdown-mode-map "r" markdown-insert-reference-link-dwim))
 
 (provide 'init-markdown)
