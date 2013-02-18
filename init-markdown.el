a3e77cab7664b19f8093efd8666f8c2ca07b0344
John Mastro <john.b.mastro@gmail.com>
2013-02-17 16:12:00 -0800

    Update init-markdown.el

--- init-markdown.el
+++ init-markdown.el
@@ -6,13 +6,17 @@
           (add-to-list 'auto-mode-alist (cons ext 'markdown-mode)))
       (list "\\.markdown" "\\.mkd" "\\.md"))
 
-(defun maybe-turn-on-auto-fill ()
+(defun basis/init-markdown-mode ()
   (unless (eq major-mode 'gfm-mode)
-    (turn-on-auto-fill)))
+    (turn-on-auto-fill))
+  (define-key markdown-mode-map
+      (kbd "C-c r") 'markdown-insert-reference-link-dwim))
 
-(add-hook 'markdown-mode-hook 'maybe-turn-on-auto-fill)
+(defun basis/init-gfm-mode ()
+  (define-key gfm-mode-map
+      (kbd "C-c r")'markdown-insert-reference-link-dwim))
 
-(eval-after-load 'markdown
-  '(basis/define-hyper markdown-mode-map "r" markdown-insert-reference-link-dwim))
+(add-hook 'markdown-mode-hook 'basis/init-markdown-mode)
+(add-hook 'gfm-mode-hook 'basis/init-gfm-mode)
 
 (provide 'init-markdown)
