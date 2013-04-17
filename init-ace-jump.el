a007fe5615afb3bdd7d0914fb98311dcfad5929e
John Mastro <john.b.mastro@gmail.com>
2013-04-17 14:47:25 -0700

    Another key for ace-jump-mode

--- init-ace-jump.el
+++ init-ace-jump.el
@@ -6,6 +6,7 @@
   '(ace-jump-mode-enable-mark-sync))
 
 (global-set-key (kbd "M-SPC") 'ace-jump-mode)
+(global-set-key (kbd "s-SPC") 'ace-jump-mode)
 (global-set-key (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
 
 (provide 'init-ace-jump)
