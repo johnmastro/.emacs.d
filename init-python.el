287a2d88f4b79dac8fded4d45353a7f4f1c843f5
John Mastro <john.b.mastro@gmail.com>
2013-04-28 13:51:11 -0700

    Function for inserting Python modeline/shebang

--- init-python.el
+++ init-python.el
@@ -3,9 +3,21 @@
 ;; -----------------------------------------------------------------------------
 
 (defun basis/python-setup ()
-  (electric-indent-mode -1)
   (define-key python-mode-map "\C-m" 'newline-and-indent))
 
 (add-hook 'python-mode-hook 'basis/python-setup)
+
+(defun basis/insert-python-header (&optional arg)
+  "Insert a Python coding cookie and modeline.
+By default insert at the beginning of the buffer. With
+a prefix arg insert wherever point is."
+  (interactive "P")
+  (save-excursion
+    (unless arg
+      (goto-char (point-min)))
+    (insert (concatenate 'string
+                         "# -*- coding: utf-8 -*-\n"
+                         "#!/usr/bin/env python\n"
+                         "\n"))))
 
 (provide 'init-python)
