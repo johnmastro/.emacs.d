f77fcf2e555aecbc9787f7b2fe72fb2ae788cf8d
John Mastro <john.b.mastro@gmail.com>
2013-05-20 15:56:21 -0700

    Replace minor mode names with Greek letters

--- init-interface.el
+++ init-interface.el
@@ -17,11 +17,23 @@
 (when (and (display-graphic-p) basis/default-font)
   (set-face-attribute 'default nil :font basis/default-font))
 
+(eval-after-load 'paredit
+  '(diminish 'paredit-mode " π"))            ; pi
+(eval-after-load 'auto-complete
+  '(diminish 'auto-complete-mode " α"))      ; alpha
+(eval-after-load 'yasnippet
+  '(diminish 'yas-minor-mode " υ"))          ; upsilon
+(eval-after-load 'redshank
+  '(diminish 'redshank-mode " ρ"))           ; rho
+(eval-after-load 'autopair
+  '(diminish 'autopair-mode " φ"))           ; psi
+(eval-after-load 'whitespace
+  '(diminish 'whitespace-mode " ϝ"))         ; digamma
 (eval-after-load 'elisp-slime-nav
-  '(diminish 'elisp-slime-nav-mode))
+  '(diminish 'elisp-slime-nav-mode " ε"))    ; epsilon
 (eval-after-load 'eldoc
-  '(diminish 'eldoc-mode))
+  '(diminish 'eldoc-mode " δ"))              ; delta
 (eval-after-load 'undo-tree
-  '(diminish 'undo-tree-mode))
+  '(diminish 'undo-tree-mode " τ"))          ; tau
 
 (provide 'init-interface)
