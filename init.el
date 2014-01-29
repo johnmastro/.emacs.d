838d53e9666097a50ee2abab4527dab064d368eb
John Mastro <john.b.mastro@gmail.com>
2014-01-29 15:13:23 -0800

    Change binding for execute-extended-command

--- init.el
+++ init.el
@@ -954,7 +954,7 @@
 
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+(global-set-key (kbd "C-h M-x") 'execute-extended-command)
 
 ;; hippie expand ---------------------------------------------------------------
 
