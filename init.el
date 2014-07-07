13b78a1d6f87216c46a0e2e3ed7d6574840b2d07
John Mastro <john.b.mastro@gmail.com>
2014-07-07 14:37:11 -0700

    Change relative order of $PATH

--- init.el
+++ init.el
@@ -138,7 +138,7 @@
 
 (defun basis/init-for-cygwin ()
   (require 'cygwin-mount)
-  (let* ((dirs (->> '("/bin" "/usr/bin" "/usr/local/bin"
+  (let* ((dirs (->> '("/usr/local/bin" "/bin" "/usr/bin"
                       "/Python27" "/Python27/Scripts")
                  (-filter #'file-directory-p)))
          (home (getenv "HOME"))
