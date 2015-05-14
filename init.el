060d567cff8a90cc1e165359b5ca6f198b5df763
John Mastro <john.b.mastro@gmail.com>
2015-05-14 16:24:02 -0700

    Set magit-revert-buffers to `silent'

--- init.el
+++ init.el
@@ -1180,6 +1180,7 @@ Return the empty string (i.e. get rid of the help string)."
   (unless (boundp 'magit-backup-mode)
     ;; Temporary kludge to prevent an unbound-symbol error
     (setq magit-backup-mode nil))
+  (setq magit-revert-buffers 'silent)
   (setq magit-completing-read-function #'magit-ido-completing-read)
   (setq magit-repository-directories
         (thread-last (projectile-relevant-known-projects)
